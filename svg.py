d5b9d14e505c201aaf853e9030dc5e2ef9c97246
Cornelius Schumacher <schumacher@kde.org>
2019-01-06 02:00:04 +0100

    Extract methods

--- svg.py
+++ svg.py
@@ -20,6 +20,8 @@ class Document:
         self.last_level = 0
         self.out = ""
         self.open_tag = False
+        self.text('<?xml version="1.0" standalone="no"?>')
+        self.text('<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">')
 
     def indent(self):
         self.out += self.level * '  '
@@ -48,39 +50,32 @@ class Document:
         self.indent()
         self.out += text + "\n"
 
+def write_text(doc, text, size=20, fill="black", x="0", y="0"):
+    with doc.tag('g', 'font-size="%s" font-family="arial" fill="%s"' % (size, fill)):
+        with doc.tag('text', 'x="%s" y="%s"' % (x, y)):
+            doc.text(text)
+
+def write_rect(doc, x=0, y=0, width=10, height=10, fill="black"):
+    doc.tag('rect', 'x="%s" y="%s" width="%s" height="%s" fill="%s" rx="5"' % (x, y, width, height, fill))
+
 def write_svg(labels, filename):
     doc = Document()
-
-    doc.text('<?xml version="1.0" standalone="no"?>')
-    doc.text('<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">')
     with doc.tag('svg', 'width="600" height="400"'):
         doc.tag('rect', 'x="0" y="0" width="600" height="220" fill="#eee"')
 
-        with doc.tag('g', 'font-size="30" font-family="arial" fill="#444"'):
-            with doc.tag('text', 'x="40" y="50"'):
-                doc.text("Labels for someorg/somerepo")
+        write_text(doc, "Labels for %s/%s" % (labels.org, labels.repo), size=30, fill="#444", x=40, y=50)
 
-        with doc.tag('g', 'font-size="30" font-family="arial" fill="#777"'):
-            with doc.tag('text', 'x="40" y="120"'):
-                doc.text("Type")
+        write_text(doc, "Type", size=30, fill="#777", x=40, y=120)
 
-        doc.tag('rect', 'x="250" y="90" width="120" height="40" fill="#d73a4a" rx="5"')
-        with doc.tag('g', 'font-size="20" font-family="arial" fill="white"'):
-            with doc.tag('text', 'x="263" y="116"'):
-                doc.text("bug")
+        write_rect(doc, x=250, y=90, width=120, height=40, fill="#d73a4a")
+        write_text(doc, "bug", fill="white", x=263, y=116)
 
-        with doc.tag('g', 'font-size="30" font-family="arial" fill="#777"'):
-            with doc.tag('text', 'x="40" y="180"'):
-                doc.text("Components")
+        write_text(doc, "Components", size=30, fill="#777", x=40, y=180)
 
-        doc.tag('rect', 'x="250" y="150" width="120" height="40" fill="#EE7912" rx="5"')
-        with doc.tag('g', 'font-size="20" font-family="arial" fill="white"'):
-            with doc.tag('text', 'x="263" y="176"'):
-                doc.text("frontend")
-        doc.tag('rect', 'x="390" y="150" width="120" height="40" fill="#123456" rx="5"')
-        with doc.tag('g', 'font-size="20" font-family="arial" fill="white"'):
-            with doc.tag('text', 'x="403" y="176"'):
-                doc.text("backend")
+        write_rect(doc, x=250, y=150, width=120, height=40, fill="#EE7912")
+        write_text(doc, "frontend", fill="white", x=263, y=176)
+        write_rect(doc, x=390, y=150, width=120, height=40, fill="#123456")
+        write_text(doc, "backend", fill="white", x=403, y=176)
 
     with open(filename, "w") as file:
         file.write(doc.out)
--- test_svg.py
+++ test_svg.py
@@ -12,7 +12,9 @@ def test_generate_xml():
 
     print(doc.out)
 
-    assert doc.out == '''<svg height="400" width="800" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    assert doc.out == '''<?xml version="1.0" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg height="400" width="800" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
   <rect y="20"/>
   <rect y="30"/>
   <g x="20">
