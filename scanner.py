031880ca19fef5cee94384be7c1d347ff25c2832
Cornelius Schumacher <schumacher@kde.org>
2019-01-10 13:45:05 +0100

    Make it possible to supply an authentication token

--- scanner.py
+++ scanner.py
@@ -1,6 +1,8 @@
 import subprocess
 import json
 from pathlib import Path
+import os
+import sys
 
 from labels import Labels
 
@@ -12,8 +14,11 @@ def scan_labels(config):
         with open(config.json_filename(), 'r') as file:
             json_data = file.read()
     else:
-        json_data = subprocess.check_output(['curl', '-s', '-H "Accept: application/vnd.github.symmetra-preview+json"',
-            'https://api.github.com/repos/' + config.org + '/' + config.repo + '/labels']).decode('utf-8')
+        command = ['curl', '-s', '-HAccept: application/vnd.github.symmetra-preview+json']
+        if os.environ['GITHUB_TOKEN']:
+            command += ['-HAuthorization: token %s' % os.environ['GITHUB_TOKEN']]
+        command += ['https://api.github.com/repos/' + config.org + '/' + config.repo + '/labels']
+        json_data = subprocess.check_output(command).decode('utf-8')
         with open(config.json_filename(), 'w') as file:
             file.write(json_data)
 
@@ -21,6 +26,10 @@ def scan_labels(config):
 
 def labels_from_json_data(org, repo, json_data):
     labels_json = json.loads(json_data)
+    if "message" in labels_json:
+        sys.exit("Error retrieving labels from GitHub: " + labels_json["message"] +
+                "\nIf you are accessing a private repository you need to set a GitHub token "
+                "in the environment variable GITHUB_TOKEN.")
     labels = Labels(org, repo)
     for label in labels_json:
         labels.add_label('scanned', label['id'], label['name'], label['description'], label['color'])
